64dc02ca99542cbe93fef96ab983222791e8d873
Oscar Alejandro Rafael Cabanillas <oscar.cabanillas@mercadolibre.com>
2022-05-30 15:39:03 -0300

    Finished Exercise 3

--- AlkeParking.playground/Contents.swift
+++ AlkeParking.playground/Contents.swift
@@ -5,6 +5,8 @@ import UIKit
 protocol Parkable {
     var plate: String { get }
     var type: VehicleType { get }
+    var checkInTime: Date { get }
+    var discountCard: String? { get }
 }
 
 struct Parking {
@@ -14,6 +16,8 @@ struct Parking {
 struct Vehicle: Parkable, Hashable {
     let plate: String
     let type: VehicleType
+    var checkInTime: Date
+    var discountCard: String?
     
     static func == (lhs: Vehicle, rhs: Vehicle) -> Bool {
         return lhs.plate == rhs.plate
