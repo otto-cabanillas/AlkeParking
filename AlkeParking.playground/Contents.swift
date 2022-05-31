634dbc1ef4889e6427f3c86564efde0a64a6a8f2
Oscar Alejandro Rafael Cabanillas <oscar.cabanillas@mercadolibre.com>
2022-05-31 12:54:08 -0300

    Finished Exercise 8

--- AlkeParking.playground/Contents.swift
+++ AlkeParking.playground/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Foundation
 
-// MARK: AlkeParking Exercise 7
+// MARK: AlkeParking Exercise 8
 
 protocol Parkable {
     var plate: String { get }
@@ -25,9 +25,22 @@ struct Parking {
         guard let vehicle = vehicles.first(where: {$0.plate == plate }) else {
             return onError()
         }
+        
+        let fee = calculateFee(type: vehicle.type, parkedTime: vehicle.parkedTime)
         vehicles.remove(vehicle)
-        onSuccess(99)
+        onSuccess(fee)
     }
+    
+    func calculateFee(type: VehicleType, parkedTime: Int) -> Int {
+        var fee = type.hourFee
+        print("fee: \(fee)")
+        if parkedTime > 120 {
+            let reminderMins = parkedTime - 120
+            fee += Int(ceil(Double(reminderMins) / 15.0)) * 5
+        }
+        return fee
+    }
+    
 }
 
 struct Vehicle: Parkable, Hashable {
@@ -37,6 +50,7 @@ struct Vehicle: Parkable, Hashable {
     var discountCard: String?
     
     var parkedTime: Int {
+
         return Calendar.current.dateComponents([.minute], from: checkInTime, to: Date()).minute ?? 0
     }
     
@@ -69,6 +83,15 @@ enum VehicleType {
     }
 }
 
+extension Date {
+    static func createDate(hour: Int, mins: Int) -> Date {
+        let dayCurrent = Calendar.current.date(bySettingHour: hour, minute: mins, second: 0, of: Date())
+        let someDateTime = dayCurrent
+        return someDateTime ?? Date()
+    }
+}
+
+
 var alkeParking = Parking()
 
 // 20 Vehicles
@@ -93,7 +116,7 @@ let miniBus15 = Vehicle(plate: "CC333FF", type: .miniBus, checkInTime: Date(), d
 let bus16 = Vehicle(plate: "DD444HH", type: .bus, checkInTime: Date(), discountCard: nil)
 let bus17 = Vehicle(plate: "AA111EE", type: .bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_008")
 let bus18 = Vehicle(plate: "B222FFF", type: .bus, checkInTime: Date(), discountCard: nil)
-let bus19 = Vehicle(plate: "CC333GG", type: .bus, checkInTime: Date(), discountCard: "DISCOUNT_CARD_009")
+let bus19 = Vehicle(plate: "CC333GG", type: .bus, checkInTime: Date.createDate(hour: 10, mins: 00), discountCard: "DISCOUNT_CARD_009")
 let bus20 = Vehicle(plate: "DD444II", type: .bus, checkInTime: Date(), discountCard: nil)
 
 // Plate duplicate
@@ -118,6 +141,9 @@ for vehicle in vehicles {
 }
 
 alkeParking.vehicles.count
+
+bus19.parkedTime
+bus20.parkedTime
 
 alkeParking.checkOutVehicle(plate: "CC333GG") { Cost in
     print("Su costo fue de \(Cost)")
